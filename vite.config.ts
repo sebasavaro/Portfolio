5effd56aa2275d534bfadf03611a983898bbb044
sebasavaro <sebasavarobula@gmail.com>
2026-01-21 04:25:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Portfolio-Sebastian/', // Esta línea permite que GitHub encuentre tus archivos
+    base: '/Portfolio-Sebastian/', 
     server: {
       port: 3000,
       host: '0.0.0.0',
